2d00bf9eda77fe825e1fcdd1ecf179fd8e73fff8
unknown <wpol.reyes@gmail.com>
2024-06-22 20:13:21 -0500

    actualización de token

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -3,7 +3,7 @@ const VerifyToken = (req, res) => {
     try {
         let accesToken = "bf7886799222fe3518017ab";
         let token = req.query["hub.verify_token"];
-        let challenge = req.body["hub.challenge"];
+        let challenge = req.query["hub.challenge"];
 
         if (challenge != null && token != null && token == accesToken) {
             res.send(challenge, "0");
