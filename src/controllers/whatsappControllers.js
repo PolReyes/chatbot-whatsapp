ec5769dfae2c7a3c537d6c897905e579fb759d4f
unknown <wpol.reyes@gmail.com>
2024-06-22 19:08:40 -0500

    actualización de token y rutas

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,22 @@
 const VerifyToken = (req, res) => {
-    res.send("Token correcto");
+
+    try {
+        let accesToken = process.env.TOKEN;
+        let token = req.query["hub.verify_token"];
+        let challenge = req.body["hub.challenge"];
+
+        if (challenge != null && token != null && token == accesToken) {
+            res.send(challenge);
+        } else {
+            res.status(400).send();
+        }
+
+
+
+    } catch (e) {
+        res.status(400).send();
+    }
+
 }
 
 const ReceivedMessage = (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const apiRoute = require("./routes/routes");
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 
