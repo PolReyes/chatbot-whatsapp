3245de5262ae29cb33e2a09891a9d97294c16e63
unknown <wpol.reyes@gmail.com>
2024-06-25 10:54:58 -0500

    detectando errores

--- src/index.js
+++ src/index.js
@@ -9,8 +9,23 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/webhook', apiRoute);
+/* app.use('/webhook', apiRoute); */
 
+app.get("/webhook", (req, res) => {
+    let mode = req.query["hub.mode"];
+    let challenge = req.query["hub.challenge"];
+    let token = req.query["hub.verify_token"];
+
+
+
+    if (mode && token) {
+        if (mode === "subscribe" && token === mytoken) {
+            res.status(200).send(challenge);
+        } else {
+            res.status(403);
+        }
+    }
+});
 /*app.get('/', function (req, res) {
     res.send("ChatBot")
 })*/
