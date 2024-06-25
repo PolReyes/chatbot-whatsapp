165ada3a97f50d4deecf90dd154b4327303e9199
unknown <wpol.reyes@gmail.com>
2024-06-25 10:14:54 -0500

    detectando errores

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -21,7 +21,7 @@ const VerifyToken = (req, res) => {
             }
         }*/
 
-        res.send(JSON.stringify(req))
+        res.send(JSON.stringify(req.query))
         /*if (challenge != null && token != null && token === accesToken) {
             res.send(challenge, "0 " + mode + "//-" + challenge + "//-" + token + "//-" + process.env.TOKEN);
         } else {
