b1c01d3855d5870040b8b77c6f39dc3b4e4ea331
unknown <wpol.reyes@gmail.com>
2024-06-25 14:03:53 -0500

    configurando metodo POST

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -30,7 +30,7 @@ const VerifyToken = (req, res) => {
 const ReceivedMessage = (req, res) => {
     let body_param = req.body;
 
-    console.log(JSON.stringify(body_param, null, 2));
+    // console.log(JSON.stringify(body_param, null, 2));
 
     if (body_param.object) {
         if (body_param.entry &&
@@ -41,19 +41,19 @@ const ReceivedMessage = (req, res) => {
             let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-
+            console.log(phon_no_id + "/" + from + "/" + msg_body)
             axios({
                 method: "POST",
                 //url: ?access_token=EAAKfkGMvt58BO9ZBGCJ8odNmUZA60vtqZCwlwLbW3ppXuISJskKtpStwR6uFnx94zGxgvqyVLjvMAq0bYFZAuVYZCDSoA4Qa8LbJOWueWAi2Tcn1KJJFcoYjHPnDFiCzyB1RfCPihTeNiFE45Bph88y3QZB5RqbztemXD1vms3kYZCoBBMyZCH1nkYMeXbkFwyVQ
-                url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/messages?access_token=" + process.env.TOKENAPI,
+                url: "https://graph.facebook.com/v19.0/" + phon_no_id + "/messages?access_token=" + process.env.TOKENAPI,
                 //url: "https://graph.facebook.com/v19.0/" + phon_no_id + "/messages",
                 data: {
-                    "messaging_product": "whatsapp",
-                    "to": from,
-                    "text": {
-                        "body": "Hi.. I'm Prasath, your message is " + msg_body
+                    messaging_product: "whatsapp",
+                    to: from,
+                    text: {
+                        body: "Hi.. I'm Prasath, your message is " + msg_body
                     },
-                    "type": "text"
+                    type: "text"
                 },
                 headers: {
                     "Content-Type": "application/json",
