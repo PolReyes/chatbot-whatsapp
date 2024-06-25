67be413a67d22fc6a96624f940c68afde3697df5
unknown <wpol.reyes@gmail.com>
2024-06-25 10:57:25 -0500

    detectando errores

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const apiRoute = require("./routes/routes");
+const body_parser = require("body-parser");
+const app = express().use(body_parser.json());
 require('dotenv').config();
 
-const app = express();
+//const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.json());
+//app.use(express.json());
 
 /* app.use('/webhook', apiRoute); */
 
