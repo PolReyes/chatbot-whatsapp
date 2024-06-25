29663888ad539e0ef03d0ed844a49188a0f96e43
unknown <wpol.reyes@gmail.com>
2024-06-25 14:06:13 -0500

    configurando metodo POST

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -33,15 +33,19 @@ const ReceivedMessage = (req, res) => {
     // console.log(JSON.stringify(body_param, null, 2));
 
     if (body_param.object) {
+        console.log("inside body param");
         if (body_param.entry &&
             body_param.entry[0].changes &&
-            body_param.entry[0].changes[0].value.message &&
-            body_param.entry[0].changes[0].value.message[0]
+            body_param.entry[0].changes[0].value.messages &&
+            body_param.entry[0].changes[0].value.messages[0]
         ) {
             let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-            console.log(phon_no_id + "/" + from + "/" + msg_body)
+
+            console.log("phone number " + phon_no_id);
+            console.log("from " + from);
+            console.log("boady param " + msg_body);
             axios({
                 method: "POST",
                 //url: ?access_token=EAAKfkGMvt58BO9ZBGCJ8odNmUZA60vtqZCwlwLbW3ppXuISJskKtpStwR6uFnx94zGxgvqyVLjvMAq0bYFZAuVYZCDSoA4Qa8LbJOWueWAi2Tcn1KJJFcoYjHPnDFiCzyB1RfCPihTeNiFE45Bph88y3QZB5RqbztemXD1vms3kYZCoBBMyZCH1nkYMeXbkFwyVQ
