75fe2b106d766b2cdbab92cb8d9c06f66aa7e2ef
unknown <wpol.reyes@gmail.com>
2024-06-25 15:26:33 -0500

    configurando metodo POST

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -34,10 +34,11 @@ const ReceivedMessage = (req, res) => {
     try {
         if (body_param.object) {
             console.log("1", body_param.object);
-            if (body_param.entry &&
+            if (body_param.entry
+                /*&&
                 body_param.entry[0].changes &&
                 body_param.entry[0].changes[0].value.messages &&
-                body_param.entry[0].changes[0].value.messages[0]
+                body_param.entry[0].changes[0].value.messages[0]*/
             ) {
                 console.log("2")
                 let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
