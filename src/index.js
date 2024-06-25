2cea80b9b498f1b1880b4d037427c4a3516533ea
unknown <wpol.reyes@gmail.com>
2024-06-25 12:01:48 -0500

    detectando errores

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get("/webhook", (req, res) => {
     let challenge = req.query["hub.challenge"];
     let token = req.query["hub.verify_token"];
 
-    const data = req.query;
+    const data = req.params.token;
     console.log(data);
     return res.status(200).json(data);
     /*
