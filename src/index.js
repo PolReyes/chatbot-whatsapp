649db716d692de509572bbd13a65537fb789c766
unknown <wpol.reyes@gmail.com>
2024-06-25 11:31:45 -0500

    detectando errores

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 3000;
 /* app.use('/webhook', apiRoute); */
 
 app.get("/webhook", (req, res) => {
+    res.status(200).send(parse.JSON(req.body))
+    /*
     let mode = req.query["hub.mode"];
     let challenge = req.query["hub.challenge"];
     let token = req.query["hub.verify_token"];
@@ -26,11 +28,11 @@ app.get("/webhook", (req, res) => {
         } else {
             res.status(403);
         }
-    }
+    }*/
 });
 
 app.get('/', function (req, res) {
-    res.send("ChatBot")
+    res.status(200).send("ChatBot OK")
 })
 
 app.listen(PORT, () => (console.log("el puerto es: " + PORT)));
